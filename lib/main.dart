c6e9f5267f168b0e35c52b7dcfe6e25a78023a65
kirill <sgaska@bk.ru>
2025-10-31 22:46:05 +0300

    Инициализация DI контейнера GetIt

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
 import 'app.dart';
+import 'data/payment_repository.dart';
 
 
 void main() {
+  GetIt.I.registerSingleton<PaymentRepository>(PaymentRepository());
   runApp(const RegularPaymentsApp());
 }
