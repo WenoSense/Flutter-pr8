957d2a73f81ec351213f4a2db4da99777e68531a
kirill <sgaska@bk.ru>
2025-10-31 22:47:09 +0300

    Удален PaymentProvider

--- lib/app.dart
+++ lib/app.dart
@@ -1,23 +1,17 @@
 import 'package:flutter/material.dart';
 import 'router.dart';
 import 'shared/app_theme.dart';
-import 'data/payment_repository.dart';
-import 'features/payments/payment_provider.dart';
 
 class RegularPaymentsApp extends StatelessWidget {
   const RegularPaymentsApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final paymentRepo = PaymentRepository();
-    return PaymentProvider(
-      paymentRepo: paymentRepo,
-      child: MaterialApp.router(
-        title: 'Регулярные платежи',
-        theme: AppTheme.light,
-        debugShowCheckedModeBanner: false,
-        routerConfig: appRouter,
-      ),
+    return MaterialApp.router(
+      title: 'Регулярные платежи',
+      theme: AppTheme.light,
+      debugShowCheckedModeBanner: false,
+      routerConfig: appRouter,
     );
   }
 }
